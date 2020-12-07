65a368b05470cb0a4ab839ed30ba94c5a78ec1a6
Matt Steiner <Metropolis.4488@gmail.com>
2020-12-06 23:23:40 -0500

    added endpoints

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,6 +17,39 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+  .then(dbWork => {
+    res.json(dbWork);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+})
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/stats.html"))
+});
+
+app.put("/api/workouts/:id", ({body, params}, res) => {
+  // console.log(body)
+  // console.log(params)
+  db.Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}})
+  .then(dbExercise => {
+    res.json(dbExercise)
+  })
+  .catch(err => {
+    res.json(err)
+  })
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
